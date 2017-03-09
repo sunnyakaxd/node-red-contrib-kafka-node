bfa4a33d020b5f3b6bcf244edccaa66041dcb13e
Himanshu Mishra <himanshu.mishra@elastic.run>
2017-03-09 15:28:24 +0530

    add individual batch processing, with callbacks

---
+++ kafka-batch-runner.js
@@ -0,0 +1,83 @@
+var async = require('async');
+var kafka = require('kafka-node');
+var ConsumerGroup = kafka.ConsumerGroup;
+function Node(data) {
+  this.data = data;
+}
+function LinkedList() {
+  this.length = 0;
+    //this.head = undefined;
+    //this.tail = undefined;
+};
+LinkedList.prototype.pop = function(data) {
+  if (!this.head) {
+    return;
+  }
+  this.length--;
+  var retVal = this.head.data;
+  this.head = this.head.next;
+  return retVal;
+};
+LinkedList.prototype.add = function(data) {
+  if (data === undefined) {
+    throw new Error('Cannot insert undefined into linked list');
+  }
+  if (!this.head) {
+    this.head = this.tail = new Node(data);
+  } else {
+    this.tail = this.tail.next = new Node(data);
+  }
+  this.length++;
+};
+function KafkaBatchRunner(node, options, topics) {
+  var consumerGroup = this.consumerGroup = new ConsumerGroup(options, topics);
+  var versionDict = {};
+  versionDict.__cur = 0;
+  var running = false;
+  function next(version) {
+    if (versionDict.__cur == version) {
+      versionDict.__cur = (versionDict.__cur + 1) % 10;
+    }
+  }
+  function run() {
+    if (running) {
+      return;
+    }
+    running = true;
+    var thisRun = versionDict.__cur;
+    next(thisRun);
+    var list = versionDict[thisRun].list;
+//        var cbArray = new Array(list.length)
+    async.times(list.length, function(n, next) {
+      node.send({
+        payload: list.pop(),
+        cb: next,
+      });
+    }, function(err) {
+      if (err) {
+        process.exit(1);
+      }
+      delete versionDict[thisRun];
+      console.log('Resuming ', consumerGroup + '');
+      consumerGroup.commit();
+      consumerGroup.resume();
+      console.log('Commited and Resumed');
+    });
+  }
+  consumerGroup.on('message', function(msg) {
+    console.log('got msg');
+    var myVersion = versionDict.__cur;
+    var curvDict = versionDict[myVersion] = (versionDict[myVersion] || {
+      list: new LinkedList(),
+    });
+    curvDict.list.add({
+      msg: msg,
+    });
+    console.log('calling pause');
+    consumerGroup.pause();
+    console.log('calling run');
+    process.nextTick(run);
+  });
+}
+module.exports = KafkaBatchRunner;
+
--- kafka.js
+++ kafka.js
@@ -5,54 +5,55 @@ module.exports = function(RED) {
     /*
      *   Kafka Producer
      *   Parameters:
-     - topics 
+     - topics
      - zkquorum(example: zkquorum = “[host]:2181")
      */
-    function kafkaNode(config) {
-        RED.nodes.createNode(this,config);
-        var topic = config.topic;
-        var clusterZookeeper = config.zkquorum;
-        var debug = (config.debug == "debug");
-        var node = this;
-        var kafka = require('kafka-node');
-        var HighLevelProducer = kafka.HighLevelProducer;
-        var Client = kafka.Client;
-        var topics = config.topics;
-        var client = new Client(clusterZookeeper);
+  function kafkaNode(config) {
+    RED.nodes.createNode(this, config);
+    var topic = config.topic;
+    var clusterZookeeper = config.zkquorum;
+    var debug = (config.debug == 'debug');
+    var node = this;
+    var kafka = require('kafka-node');
+    var HighLevelProducer = kafka.HighLevelProducer;
+    var Client = kafka.Client;
+    var topics = config.topics;
+    var client = new Client(clusterZookeeper);
 
-        try {
-            this.on("input", function(msg) {
-                var payloads = [];
+    try {
+      this.on('input', function(msg) {
+        var payloads = [];
 
                 // check if multiple topics
-                if (topics.indexOf(",") > -1){
-                    var topicArry = topics.split(',');
+        if (topics.indexOf(',') > -1) {
+          var topicArry = topics.split(',');
 
-                    for (i = 0; i < topicArry.length; i++) {
-                        payloads.push({topic: topicArry[i], messages: msg.payload});
-                    }
-                }
-                else {
-                    payloads = [{topic: topics, messages: msg.payload}];
-                }
+          for (i = 0; i < topicArry.length; i++) {
+            payloads.push({topic: topicArry[i], messages: msg.payload});
+          }
+        } else {
+          payloads = [{topic: topics, messages: msg.payload}];
+        }
 
-                producer.send(payloads, function(err, data){
-                    if (err){
-                        node.error(err);
-                    }
-                    node.log("Message Sent: " + data);
-                });
-            });
-        }
-        catch(e) {
-            node.error(e);
-        }
-        var producer = new HighLevelProducer(client);
-        this.status({fill:"green",shape:"dot",text:"connected to "+clusterZookeeper});
+        producer.send(payloads, function(err, data) {
+          if (err) {
+            node.error(err);
+          }
+          node.log('Message Sent: ' + data);
+        });
+      });
+    } catch (e) {
+      node.error(e);
     }
+    var producer = new HighLevelProducer(client);
+    this.status({
+      fill: 'green',
+      shape: 'dot',
+      text: 'connected to ' + clusterZookeeper,
+    });
+  }
 
-    RED.nodes.registerType("kafka",kafkaNode);
-
+  RED.nodes.registerType('kafka', kafkaNode);
 
     /*
      *   Kafka Consumer
@@ -61,73 +62,110 @@ module.exports = function(RED) {
      - groupId
      - zkquorum(example: zkquorum = “[host]:2181")
      */
-    function kafkaInNode(config) {
-        RED.nodes.createNode(this,config);
+  function kafkaInNode(config) {
+    RED.nodes.createNode(this, config);
 
-        var node = this;
+    var node = this;
+    var highLevelConsumer = false;
+    var kafka = require('kafka-node');
+    var HighLevelConsumer = kafka.HighLevelConsumer;
+    var Client = kafka.Client;
+    var topics = String(config.topics);
+    var clusterZookeeper = config.zkquorum;
+    var groupId = config.groupId;
+    var debug = (config.debug == 'debug');
+    var client = new Client(clusterZookeeper);
+    var cgTopics = topics.split(',');
+    topics = cgTopics.map(function(topic) {
+      return {
+        topic: topic,
+      };
+    });
+//    return console.log(JSON.stringify(config));
+//        var topicJSONArry = [];
+//        // check if multiple topics
+//        if (topics.indexOf(",") > -1){
+//            var topicArry = topics.split(',');
+//            if (debug) {
+//                console.log(topicArry)
+//                console.log(topicArry.length);
+//            }
+//
+//            for (i = 0; i < topicArry.length; i++) {
+//                if (debug) {
+//                    console.log(topicArry[i]);
+//                }
+//                topicJSONArry.push({topic: topicArry[i]});
+//            }
+//            topics = topicJSONArry;
+//        }
+//        else {
+//            topics = [{topic:topics}];
+//        }
 
-        var kafka = require('kafka-node');
-        var HighLevelConsumer = kafka.HighLevelConsumer;
-        var Client = kafka.Client;
-        var topics = String(config.topics);
-        var clusterZookeeper = config.zkquorum;
-        var groupId = config.groupId;
-        var debug = (config.debug == "debug");
-        var client = new Client(clusterZookeeper);
+    if (highLevelConsumer) {
+      var options = {
+        groupId: groupId,
+        autoCommit: config.autoCommit,
+        autoCommitMsgCount: 10,
+      };
+      try {
+        var consumer = new HighLevelConsumer(client, topics, options);
+        this.log('Consumer created...');
+        this.status({
+          fill: 'green',
+          shape: 'dot',
+          text: 'connected to ' + clusterZookeeper,
+        });
 
-        var topicJSONArry = [];
+        consumer.on('message', function(message) {
+          if (debug) {
+            console.log(message);
+            node.log(message);
+          }
+          var msg = {payload: message};
+          node.send(msg);
+        });
 
-        // check if multiple topics
-        if (topics.indexOf(",") > -1){
-            var topicArry = topics.split(',');
-            if (debug) {
-                console.log(topicArry)
-                console.log(topicArry.length);
-            }
-
-            for (i = 0; i < topicArry.length; i++) {
-                if (debug) {
-                    console.log(topicArry[i]);
-                }
-                topicJSONArry.push({topic: topicArry[i]});
-            }
-            topics = topicJSONArry;
-        }
-        else {
-            topics = [{topic:topics}];
-        }
-
+        consumer.on('error', function(err) {
+          console.error(err);
+        });
+      } catch (e) {
+        node.error(e);
+        return;
+      }
+    } else {
+      try {
         var options = {
-            groupId: groupId,
-            autoCommit: config.autoCommit,
-            autoCommitMsgCount: 10
+          host: clusterZookeeper,
+          zk: {
+            sessionTimeout: 10000,
+          },   // put client zk settings if you need them (see Client)
+          batch: undefined, // put client batch settings if you need them (see Client)
+            //  ssl: true, // optional (defaults to false) or tls options hash
+          groupId: groupId,
+          sessionTimeout: 15000,
+              // An array of partition assignment protocols ordered by preference.
+              // 'roundrobin' or 'range' string for built ins (see below to pass in custom assignment protocol)
+          protocol: ['roundrobin'],
+
+              // Offsets to use for new groups other options could be 'earliest' or 'none' (none will emit an error if no offsets were saved)
+              // equivalent to Java client's auto.offset.reset
+          fromOffset: 'earliest', // default
+
+              // how to recover from OutOfRangeOffset error (where save offset is past server retention) accepts same value as fromOffset
+          outOfRangeOffset: 'earliest', // default
+          migrateHLC: false,    // for details please see Migration section below
+          migrateRolling: true,
         };
-
-
-        try {
-            var consumer = new HighLevelConsumer(client, topics, options);
-            this.log("Consumer created...");
-            this.status({fill:"green",shape:"dot",text:"connected to "+clusterZookeeper});
-
-            consumer.on('message', function (message) {
-                if (debug) {
-                    console.log(message);
-                    node.log(message);
-                }
-                var msg = {payload: message};
-                node.send(msg);
-            });
-
-            
-            consumer.on('error', function (err) {
-               console.error(err);
-            });
-        }
-        catch(e){
-            node.error(e);
-            return;
-        }
+        var kafkaBatchRunner = require('./kafka-batch-runner');
+        kafkaBatchRunner(node, options, cgTopics);
+      } catch (e) {
+        node.error(e);
+        return;
+      }
     }
+  }
 
-    RED.nodes.registerType("kafka in", kafkaInNode);
+  RED.nodes.registerType('kafka in', kafkaInNode);
 };
